233a59d525bd96192716b24723008e2c425528b4
Badgeaep <hasteyae@gmail.com>
2026-04-10 21:16:54 +0200

    Updated deployment to be instant for GUILD_ID

--- deploy-commands.js
+++ deploy-commands.js
@@ -28,7 +28,7 @@ const rest = new REST().setToken(process.env.TOKEN);
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
 		const data = await rest.put(
-			Routes.applicationCommands(process.env.CLIENT_ID),
+			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
 			{ body: commands },
 		);
 
